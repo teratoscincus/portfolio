91e561b2ab89f203792781a0272db7c26cb989c2
Max <teratoscincus@hotmail.com>
2023-05-04 15:32:00 +0200

    Implement `About` model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class About(models.Model):
+    """An about section for the website."""
+
+    featured = models.BooleanField(default=True)
+    text = models.TextField()
+
+    def save(self, *args, **kwargs):
+        """Ensure only one instance is featured and that one always is featured."""
+        if self.featured:
+            About.objects.update(featured=False)
+        else:
+            try:
+                About.objects.get(featured=True)
+            except self.DoesNotExist:
+                self.featured = True
+
+        return super().save(*args, **kwargs)
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import About
+
+
+class AboutModelTest(TestCase):
+    def setUp(self):
+        self.field_values_true = dict(
+            featured=True,
+            text="This is the About page",
+        )
+        self.field_values_false = dict(
+            featured=False,
+            text="This is the About page",
+        )
+        return super().setUp()
+
+    def test_first_always_featured(self):
+        About.objects.all().delete()
+        About.objects.create(**self.field_values_false)
+
+        self.assertTrue(About.objects.first().featured)
+
+    def test_cant_manually_remove_featured_status(self):
+        About.objects.all().delete()
+        About.objects.create(**self.field_values_true)
+        About.objects.create(**self.field_values_false)
+
+        self.assertEqual(len(About.objects.filter(featured=True)), 1)
+
+        featured_object = About.objects.get(featured=True)
+        featured_object.featured = False
+
+        self.assertEqual(About.objects.get(featured=True).pk, featured_object.pk)
+
+    def test_two_cannot_be_featured(self):
+        About.objects.all().delete()
+        About.objects.create(**self.field_values_true)
+        About.objects.create(**self.field_values_true)
+
+        first = About.objects.get(pk=1)
+        self.assertFalse(first.featured)
+        second = About.objects.get(pk=2)
+        self.assertTrue(second.featured)
