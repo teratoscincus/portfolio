0af15c787db4eedcf2d239b83b1c856363764413
Max <teratoscincus@hotmail.com>
2023-05-05 12:07:02 +0200

    Display featured `About` instance in About page

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import AboutView, IndexView
 
 app_name = "core"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("about/", AboutView.as_view(), name="about"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
 
+from .models import About
+
 
 class IndexView(TemplateView):
     template_name = "core/index.html"
+
+
+class AboutView(TemplateView):
+    template_name = "core/about.html"
+
+    def get_context_data(self, **kwargs):
+        return {"about": About.objects.get(featured=True).text}
