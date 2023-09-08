0283368f0922069293df943f2063332c4ea21989
Max <teratoscincus@hotmail.com>
2023-09-08 10:48:54 +0200

    Rename portfolio_webapp to config for clarity

--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for portfolio_webapp project.
+ASGI config for the project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio_webapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_asgi_application()
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for portfolio_webapp project.
+URL configuration for the project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
