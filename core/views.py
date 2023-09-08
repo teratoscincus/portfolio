6d4080c6c2a5c44e2a17a8356341e4534b76e955
Max <teratoscincus@hotmail.com>
2023-09-08 10:29:30 +0200

    Fix: 500 status on about page if no `About` object
    Return `None` if no `About` object has been created yet, and let the
    frond end dynamically set page content.

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import About
 
@@ -11,4 +12,8 @@ class AboutView(TemplateView):
     template_name = "core/about.html"
 
     def get_context_data(self, **kwargs):
-        return {"about": About.objects.get(featured=True).text}
+        try:
+            about = About.objects.get(featured=True).text
+        except ObjectDoesNotExist:
+            about = None
+        return {"about": about}
