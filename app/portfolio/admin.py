fe41f28f884c2be4b94339282a05d9ebd2092cb4
Max <teratoscincus@hotmail.com>
2023-10-08 19:56:36 +0200

    fix(project): Exclude `slug` field from admin form

--- app/portfolio/admin.py
+++ app/portfolio/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Project
 
-admin.site.register(Project)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    exclude = ["slug"]
